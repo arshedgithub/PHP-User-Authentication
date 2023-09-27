d97fab1aca283326adf14fa84ed677c8988d4269
arshedgithub <arshedahmedmail@gmail.com>
2023-09-27 18:54:14 +0530

    add home page

---
+++ home.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <style>
+            .menu {
+                    text-align: center;
+                    margin: auto;
+                    width: 300px;
+                    border: 1px solid black;
+                    padding: 20px;
+                }
+        </style>
+    </head>
+    <body>
+        <div class="menu">
+            <a href="home.php">Home</a><br>
+            <a href="products.php">Products</a><br>
+            <a href="logout.php">logout</a>
+        </div>
+        <h1>
+            This is HOME page.
+        </h1>
+    </body>
+</html>
