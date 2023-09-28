0a34c0e0e27ae3c0ccdbcbade4a5516c9ce71365
arshedgithub <arshedahmedmail@gmail.com>
2023-09-28 09:46:35 +0530

    add exported sql

--- info.php
+++
@@ -1,6 +0,0 @@
-<?php
-            setCookie("username",$cvalue,time()+10);
-            setCookie("name", "cooki", time()-60);
-            // print_r($_COOKIE["username"]);
-            print_r($_COOKIE);
-  ?>
